5c2632bdf06268892e812b8b863170e56e7f5769
Jhonatan Hidalgo <jshidalgop@uqvirtual.edu.co>
2020-11-10 00:35:28 -0500

    test
    test de ventanas

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="es-ES">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{--        <link rel="stylesheet" href="{{'front/css/bootstrap.min.css'}}" type="text/css">--}}
+{{--        <link rel="stylesheet" href="{{'front/css/styles.css'}}" type="text/css">--}}
+{{--        <link rel="stylesheet" href="{{'front/css/sweetalert2.min.css'}}" type="text/css">--}}
+{{--        <!-- CDN iconos-->--}}
+{{--        <script src="https://kit.fontawesome.com/cdbbd7d001.js" crossorigin="anonymous"></script>--}}
+
+        <title>Zenit</title>
+
+    </head>
+    <body>
+{{--    @include('includes.header')--}}
+    <div id="main">
+{{--        @include('includes.menu_bar')--}}
+{{--        @yield('content')--}}
+        <h1>Espero que esto siveva, te lo pido con todas las fuerzas ...</h1>
+    </div>
+
+{{--    <script src="{{asset('front/js/jquery-3.5.1.slim.min.js')}}"></script>--}}
+{{--    <script src="{{asset('front/js/jquery-3.5.1.min.js')}}"></script>--}}
+{{--    <script src="{{asset('front/js/bootstrap.min.js')}}"></script>--}}
+{{--    <script src="{{asset('front/js/popper.min.js')}}"></script>--}}
+{{--    <script src="{{asset('front/js/all_functions.js')}}"></script>--}}
+{{--    <script src="{{asset('front/js/sweetalert2.min.js')}}"></script>--}}
+{{--    @yield('scripts')--}}
+    </body>
+</html>
