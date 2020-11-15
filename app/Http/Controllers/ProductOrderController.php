2f00281ac663e6effbc71005e976b5334005144f
Jhonatan Hidalgo <jshidalgop@uqvirtual.edu.co>
2020-11-15 16:47:14 -0500

    ajustes

--- app/Http/Controllers/ProductOrderController.php
+++ app/Http/Controllers/ProductOrderController.php
@@ -5,81 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\Product_order;
 use Illuminate\Http\Request;
 
+/**
+ * Class ProductOrderController
+ * @package App\Http\Controllers
+ */
 class ProductOrderController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Product_order  $product_order
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Product_order $product_order)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Product_order  $product_order
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Product_order $product_order)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Product_order  $product_order
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Product_order $product_order)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Product_order  $product_order
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Product_order $product_order)
-    {
-        //
-    }
 }
